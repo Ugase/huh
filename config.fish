7c1a9b1a72a82452864ca0bef9167975dac236ca
Ugase <106616134+Ugase@users.noreply.github.com>
2024-08-30 16:07:49 +0300

    Add files via upload

---
+++ config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+end
+
+function fish_greeting
+end
+alias ls="eza -SlhA"
+alias edit_fish="nvim ~/.config/fish/config.fish"
+export PATH="/home/denzel/.local/bin:/home/denzel/.pbin:$PATH"
+# cat ~/.cache/wal/sequences
+# fzf_key_bindings
+fastfetch
+# /root/.autojump/share/autojump/autojump.fish
