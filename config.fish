739be2c6bb5995f7a9bd20c850231d1f3f408ba2
Ugase <106616134+Ugase@users.noreply.github.com>
2024-09-01 23:58:11 +0300

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,12 +1,7 @@
 if status is-interactive
 end
-
-function fish_greeting
-end
+set fish_greeting
 alias ls="eza -SlhA"
 alias edit_fish="nvim ~/.config/fish/config.fish"
 export PATH="/home/denzel/.local/bin:/home/denzel/.pbin:$PATH"
-# cat ~/.cache/wal/sequences
-# fzf_key_bindings
 fastfetch
-# /root/.autojump/share/autojump/autojump.fish
