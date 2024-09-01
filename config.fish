2977d7d1e911af1f9bf24b4e140a3e4b363b0b77
Ugase <106616134+Ugase@users.noreply.github.com>
2024-09-02 01:02:03 +0300

    Update config.fish

--- config.fish
+++ config.fish
@@ -3,5 +3,5 @@ end
 set fish_greeting
 alias ls="eza -SlhA"
 alias edit_fish="nvim ~/.config/fish/config.fish"
-export PATH="/home/denzel/.local/bin:/home/denzel/.pbin:$PATH"
+export PATH="~/.local/bin:~/.pbin:$PATH"
 fastfetch
