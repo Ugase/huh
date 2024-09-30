32c2927b2701af7d128f06fa33a43603ea425e63
Ugase <106616134+Ugase@users.noreply.github.com>
2024-09-29 22:17:37 -0700

    fishy one

--- fish/config.fish
+++ fish/config.fish
@@ -8,5 +8,6 @@ export BROWSER='zen-browser'
 alias ls="eza -lhA"
 alias edit_fish="nvim ~/.config/fish/config.fish"
 set huh $HOME/huh_project/
+set -U pure_enable_single_line_prompt true
 export PATH="~/.local/bin:~/.pbin:/home/denzel/.cargo/bin/:$PATH"
 
